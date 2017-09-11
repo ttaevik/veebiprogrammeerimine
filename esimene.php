f09a3a5d0b73900d467889b168c64ae509b7de96
Tauri Taevik <ttaevik@users.noreply.github.com>
2017-09-11 14:43:02 +0300

    lisatud esimene muudatud

--- esimene.php
+++ esimene.php
@@ -10,6 +10,14 @@
 	<h1> Tauri Taevik</h1>
 	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
 	<p>Minu nimi on Tauri Taevik, olen Tallinna Ülikooli õpilane ning õpin informaatikat</p>
+	<?php
+		echo "<p>kõige esimene PHP abil väljastatud sõnum.</p>";
+		echo "<p> Täna on ";
+		echo date("d.m.Y");
+		echo ".</p>";
+		echo "<p> Lehe avamise hetkel oli kell " .date("H:i:s") .".</p>";
+		
+	?>
 </body>
 
 
