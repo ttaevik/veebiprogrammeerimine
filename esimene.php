45fc976f2e184f86b4838a85442928b886525d1e
Tauri Taevik <ttaevik@users.noreply.github.com>
2017-09-11 13:48:55 +0300

    esimene PHP fail, kus on vaid HTML

---
+++ esimene.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Tauri Taevik veebiprogremise asjad
+	</title>
+</head>
+<body>
+	<h1> Tauri Taevik</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
+	<p>Minu nimi on Tauri Taevik, olen Tallinna Ülikooli õpilane ning õpin informaatikat</p>
+</body>
+
+
+</html>
